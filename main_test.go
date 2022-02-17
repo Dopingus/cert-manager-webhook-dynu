fe5b4b7e7a3e3e6893945126a87b75ef931b2285
Robert Baumgartner <rbaumgar@redhat.com>
2022-02-17 17:39:18 +0100

    update modulename update for the test suite

--- main_test.go
+++ main_test.go
@@ -5,6 +5,9 @@ import (
 	"testing"
 
 	"github.com/jetstack/cert-manager/test/acme/dns"
+	"io/ioutil"
+	extapi "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	"log"
 )
 
 var (
@@ -16,6 +19,10 @@ func TestRunsSuite(t *testing.T) {
 	// snippet of valid configuration that should be included on the
 	// ChallengeRequest passed as part of the test cases.
 	//
+	d, err := ioutil.ReadFile("testdata/dynu/config.json")
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	// Uncomment the below fixture when implementing your custom DNS provider
 	fixture := dns.NewFixture(&dynuDNSProviderSolver{},
@@ -24,7 +31,10 @@ func TestRunsSuite(t *testing.T) {
 		dns.SetManifestPath("testdata/dynu"),
 		dns.SetBinariesPath("_test/kubebuilder/bin"),
 		dns.SetUseAuthoritative(true),
-		dns.SetDNSServer("ns4.dynu.com:53"),
+		//dns.SetDNSServer("ns4.dynu.com:53"),
+		dns.SetManifestPath("testdata/dynu/dynu-secret.yaml"),
+		dns.SetConfig(&extapi.JSON{ Raw: d, }),
+
 	)
 
 	fixture.RunConformance(t)
